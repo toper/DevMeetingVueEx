7654e166e58b64abafdf8582aff15569fad5aad0
Jarosław Karlik <jarek.karlik@gmail.com>
2019-04-13 13:44:50 +0200

    [#3.2] Add Optionvote

---
+++ VueAdvance/src/components/VoteOptions.vue
@@ -0,0 +1,56 @@
+<template>
+  <label>
+    {{label}}
+    <ul>
+      <slot v-for="el in options" name="item" :el="el">
+        <li :key="el.id">{{ el.text }}</li>
+      </slot>
+    </ul>
+  </label>
+</template>
+
+<script>
+export default {
+  Name: "VoteOptions",
+  inheritAttrs: false,
+  components: {},
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    label: {
+      type: String,
+      default: "Default label"
+    },
+    options: {
+      type: Array,
+      default: () => [
+        {
+          id: 0,
+          text: "Eat pizza",
+          createdAt: "2019-02-02T12:43:35.404Z"
+        },
+        {
+          id: 1,
+          text: "Eat burger",
+          createdAt: "2019-02-02T12:43:35.404Z"
+        }
+      ]
+    }
+  },
+  data: () => ({}),
+  computed: {
+    // listeners() {
+    //   const { click, ...listeners } = this.$listeners;
+    //   return listeners;
+    // },
+  },
+  methods: {
+    //3/ 8. Input is explicitly defined to properly update value binded as v-model
+    customInput(event) {
+      this.$emit("click", event.target.value);
+    }
+  }
+};
+</script>
--- VueAdvance/src/views/Home.vue
+++ VueAdvance/src/views/Home.vue
@@ -1,35 +1,53 @@
 <template>
   <div class="home">
-    
-    <BaseButton @click="Fire" label="Big fucking BUTTON" :isLoading="loading">
-      {{textValue}}
-    </BaseButton>
+    <BaseButton @click="Fire" label="Big fucking BUTTON" :isLoading="loading">{{textValue}}</BaseButton>
+    <br>
+    <br>
+    <VoteOptions :options="options">
+      <template v-slot:item="{ el }">
+        <div style="color: red;">- {{el.id}} {{el.text}}</div>
+      </template>
+    </VoteOptions>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import BaseButton from '@/components/BaseButton.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import VoteOptions from "@/components/VoteOptions.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld,
-    BaseButton
+    BaseButton,
+    VoteOptions
   },
   data: () => ({
-    textValue: 'Nacisnij mnie',
-    loading: false
+    textValue: "Nacisnij mnie",
+    loading: false,
+    options: [
+      {
+        id: 0,
+        text: "Text1",
+        createdAt: "2019-04-02T12:43:35.404Z"
+      },
+      {
+        id: 1,
+        text: "Text5",
+        createdAt: "2019-04-02T12:43:35.404Z"
+      }
+    ]
   }),
   methods: {
     Fire() {
       console.log("fire");
-      setTimeout(()=>{
-        this.loading = false
+      setTimeout(() => {
+        this.loading = false;
       }, 3000);
       this.loading = true;
     }
   }
-}
+};
 </script>
